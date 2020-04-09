2905e5597be8b44519b3a6998bba8c65f27349eb
Fabien Chaillou <fgchaillou@gmail.com>
2020-04-09 18:29:13 +0200

    feat(init): Init project

---
+++ README.md
@@ -0,0 +1,14 @@
+My first app developped with [react](https://fr.reactjs.org/) & [redux](https://redux.js.org/)
+
+this project just requires to install [NodeJs](https://nodejs.org/en/) in local environment
+
+## Install
+
+`npm i && npm start`
+
+Open [localhost](http://localhost:3000) to view it in the browser.
+
+
+Enjoy!
+
+![aliens go home](alien-go-home.png)
---
+++ src/reducers/index.js
@@ -0,0 +1,34 @@
+import { MOVE_OBJECTS, START_GAME, SHOOT } from '../actions';
+import moveObjects from './moveObjects';
+import startGame from './startGame';
+import shoot from './shoot';
+
+const initialGameState = {
+  started: false,
+  kills: 0,
+  lives: 3,
+  flyingObjects: [],
+  lastObjectCreatedAt: new Date(),
+  cannonBalls: [],
+
+};
+
+const initialState = {
+  angle: 45,
+  gameState: initialGameState,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case MOVE_OBJECTS:
+      return moveObjects(state, action);
+    case START_GAME:
+      return startGame(state, initialGameState);
+    case SHOOT:
+      return shoot(state, action)
+    default:
+      return state;
+  }
+}
+
+export default reducer;
